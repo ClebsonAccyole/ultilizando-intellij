6955b0f14d7a82e2be444c6bc18d35e9c7300da8
ClebsonAccyole <accyoleclebson@gmail.com>
2022-04-28 16:58:29 -0300

    criando classe Filme

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,9 +5,45 @@ import br.com.dio.model.Cachorro;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
-
         System.out.println(cachorro);
+
+        Filme filme = new Filme("A Rede Social", 2010);
+        System.out.println(filme);
 
     }
 
+}
+
+class Filme {
+    private String nome;
+    private Integer anoLanc;
+
+    public Filme(String nome, Integer anoLanc) {
+        this.nome = nome;
+        this.anoLanc = anoLanc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getAnoLanc() {
+        return anoLanc;
+    }
+
+    public void setAnoLanc(Integer anoLanc) {
+        this.anoLanc = anoLanc;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", anoLanc=" + anoLanc +
+                '}';
+    }
 }
